a1956ea163530faebc294efc3465d0332211105c
Neethu <neethu.somasekharan@sreyo.com>
2022-01-05 20:05:18 -0800

    test

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
   cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets 
+  subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
 
